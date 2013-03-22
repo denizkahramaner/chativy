753ae756441992001fccc27a7310fffbaccb7a2d
Deniz Kahramaner <dennizk@gmail.com>
2013-03-22 08:10:52 -0700

    Dashboard completely redesigned. A little more work left but it is close to completion

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
 	def check_logged_in
 		if session[:user] then
 			@logged_in_user = User.find(session[:user])
+			@user_is_tutor = session[:user_type] == "tutor"
+			@user_is_student = session[:user_type] == "student"
 		end
 	end
 
@@ -12,11 +14,6 @@ class ApplicationController < ActionController::Base
 	def redirect_if_not_logged_in
 		if !@logged_in_user then
 			redirect_to :controller => "user", :action => "login"
-		end
-	end
-
-	def user_is_tutor
-		if session[:user_type] == "tutor"
 		end
 	end
 end
