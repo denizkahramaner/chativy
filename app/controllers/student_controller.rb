cc7598960ec3ab1d2ca9ee4fd3b4d587ffc11247
Maurizio Calo Caligaris <mauriziocalo07@gmail.com>
2013-03-22 07:50:14 -0700

    conference controller

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -5,12 +5,8 @@ class StudentController < ApplicationController
 	OTSDK = OpenTok::OpenTokSDK.new API_KEY, API_SECRET
 	THIRTY_DAYS = 2592000#thirty days (max expiration date) in seconds 
 	def conference
-		# Creating Session object with p2p enabled
-		sessionProperties = {OpenTok::SessionPropertyConstants::P2P_PREFERENCE => "enabled"}    # or disabled
-		@sessionID = OTSDK.createSession( request.ip, sessionProperties )
-		expire_time = Time.now.to_i + THIRTY_DAYS
-		@token = OTSDK.generateToken :session_id => @sessionID, :role => OpenTok::RoleConstants::PUBLISHER, :expire_time => expire_time
-		@apiKey = API_KEY
+		@sessionID = params[:sessionID]
+		@token = params[:token]
 	end
 
 	def generateSession
