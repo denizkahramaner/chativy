75ddd4eb933fa0ba4fd9de023262771896897b47
Deniz Kahramaner <dennizk@gmail.com>
2013-03-22 09:36:27 -0700

    More bugfixes and refinements

--- app/controllers/information_controller.rb
+++ app/controllers/information_controller.rb
@@ -1,2 +1,13 @@
 class InformationController < ApplicationController
+	def index
+		@logged_in_user = User.find(session[:user])
+		@user_is_tutor = session[:user_type] == "tutor"
+		@user_is_student = session[:user_type] == "student"
+
+		if @logged_in_user && @user_is_tutor
+			redirect_to :controller => "tutor", :action => "dashboard"
+		elsif @logged_in_user && @user_is_student
+			redirect_to :controller => "student", :action => "dashboard"
+		end
+	end
 end
