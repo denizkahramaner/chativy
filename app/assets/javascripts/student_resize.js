51a791a0493b54b7dd25a9ea095e2c4a5ca043b8
Deniz Kahramaner <dennizk@gmail.com>
2013-03-22 13:19:50 -0700

    More iPhone integration

--- app/assets/javascripts/student_resize.js
+++ app/assets/javascripts/student_resize.js
@@ -8,10 +8,16 @@
    // RESIZES ELEMENTS according to screen size (iphone compatibility)
   function resizeElements()
   {
+
     var newwidth = $(window).width();
     var newheight = $(window).height();  
     // iphone size
-    if (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent))
+    if(/(iPhone|iPod|iPad).*AppleWebKit/i.test(navigator.userAgent) || newwidth<400)
+    {
+      $(".btn-large").css('width', '240px');
+      $(".larger-button").css('width', '240px');
+    }
+    else if (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent))
     {
       $(".btn-large").css('width', '240px');
       $(".larger-button").css('width', '240px');
@@ -19,6 +25,6 @@
     else
     {
       $(".btn-large").css('width', '380px');
-      $(".larger-button").css('width', '600px');
+      $(".larger-button .btn-large").css('width', '600px');
     }
   }
